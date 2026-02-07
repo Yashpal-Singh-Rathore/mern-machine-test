3decf33bb2529fd5e28371a33a3d58f5a1b0f386
Yashpal <singhyashpal52001@gmail.com>
2026-02-08 04:38:39 +0530

    Add MongoDB Atlas connection configuration

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
 import indexRoutes from "./src/routes/index.routes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+// connect to database
+connectDB();
 
 // mount routes
 app.use("/", indexRoutes);
